744963f2332bf84715750ae5e686c5ff759e3492
Gustavo Vinicius Dantas Ribeiro Santos <guvinidantas@gmail.com>
2024-06-04 20:35:26 -0300

    refactor: hash senha do candidato

--- src/main/java/br/com/gustavo/gestao_vagas/modules/candidate/services/CreateCandidateService.java
+++ src/main/java/br/com/gustavo/gestao_vagas/modules/candidate/services/CreateCandidateService.java
@@ -1,6 +1,7 @@
 package br.com.gustavo.gestao_vagas.modules.candidate.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.gustavo.gestao_vagas.modules.candidate.entity.CandidateEntity;
@@ -12,11 +13,17 @@ public class CreateCandidateService {
     @Autowired
     private CandidateRepository repository;
 
+    @Autowired
+    private PasswordEncoder encoder;
+
     public CandidateEntity execute(CandidateEntity candidate) throws Exception {
         var candidateFound = this.repository.findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail());
         if(candidateFound != null) {
             throw new Exception("Usuário já existe");
         }
+
+        var password = encoder.encode(candidate.getPassword());
+        candidate.setPassword(password);
 
         return this.repository.save(candidate);
     }
